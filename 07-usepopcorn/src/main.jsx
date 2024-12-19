a08c55853eda8dd94b4d6fff30579fd96457daf1
dahu0422 <149865122+dahu0422@users.noreply.github.com>
2024-12-19 02:16:18 +0000

    feat:Thinking in React: Components, Composition, and Resuability

--- 07-usepopcorn/src/main.jsx
+++ 07-usepopcorn/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
+import StarRating from './StarRating.jsx'
+import { useState } from 'react'
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0)
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <StarRating maxRating={5} message={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+    <StarRating size={24} color="blue" className='' defaultRating={3} />
+    <Test />
   </StrictMode>,
 )
