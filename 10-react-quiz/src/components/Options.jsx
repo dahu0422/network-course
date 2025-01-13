1e2cb64b01bee937ba8af8a04057f8b13e283f7d
dahu0422 <149865122+dahu0422@users.noreply.github.com>
2025-01-13 09:31:45 +0000

    feat: 10-react-quiz

---
+++ 10-react-quiz/src/components/Options.jsx
@@ -0,0 +1,13 @@
+import Questions from "./Questions";
+
+function Options({ question }) {
+    return (
+        <div className="options">
+            {question.options.map((option) => (
+                <button className="btn btn-option" key={option}>{option}</button>
+            ))}
+        </div>
+    )
+}
+
+export default Options
---
+++ 10-react-quiz/src/components/Questions.jsx
@@ -0,0 +1,12 @@
+import Options from './Options'
+
+function Questions({ question }) {
+  console.log(question)
+
+  return <div>
+    <h4>{question.question}</h4>
+    <Options question={question} />
+  </div>;
+}
+
+export default Questions;
