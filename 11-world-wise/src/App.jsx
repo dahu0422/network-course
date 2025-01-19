1588a6ed00139e315f6fbb6f5474802df1878d1b
Dahu-0712 <419460476@qq.com>
2025-01-19 22:56:37 +0800

    feat:React Route

--- 11-world-wise/src/App.jsx
+++ 11-world-wise/src/App.jsx
@@ -1,4 +1,19 @@
-export default function App() {
-  return <div>world wise</div>
-}
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
+import HomePage from "./page/HomePage"
+import Pricing from "./page/Pricing"
+import Product from "./page/Product"
+import PageNotFound from "./page/PageNotFound"
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="pricing" element={<Pricing />}></Route>
+        <Route path="Product" element={<Product />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+    </Router>
+  )
+}
---
+++ 11-world-wise/src/page/HomePage.jsx
@@ -0,0 +1,14 @@
+import PageNav from "../components/PageNav"
+import AppNav from "../components/AppNav"
+
+function HomePage() {
+  return (
+    <div>
+      <AppNav />
+      <PageNav />
+      HomePage
+    </div>
+  )
+}
+
+export default HomePage
