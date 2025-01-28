a5c394687fe38f2c6da554a42173e594279cb5b5
Dahu-0712 <419460476@qq.com>
2025-01-28 18:42:11 +0800

    feat:React Route

--- 11-world-wise/src/App.jsx
+++ 11-world-wise/src/App.jsx
@@ -1,17 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
-import HomePage from "./page/HomePage"
+import AppLayout from "./page/AppLayout"
+import HomePage from "./page/Homepage"
 import Pricing from "./page/Pricing"
 import Product from "./page/Product"
+import Login from "./page/Login"
 import PageNotFound from "./page/PageNotFound"
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
+        <Route index element={<HomePage />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
         <Route path="Product" element={<Product />}></Route>
+        <Route path="Login" element={<Login />}></Route>
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>}></Route>
+          <Route path="cities" element={<p>List of cities</p>}></Route>
+          <Route path="countries" element={<p>Countries</p>}></Route>
+          <Route path="form" element={<p>Form</p>}></Route>
+        </Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
     </Router>
