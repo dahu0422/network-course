ccd50e7b17d58e826e65ba404ec440ab3cee8a4a
Dahu-0712 <419460476@qq.com>
2025-02-25 22:54:07 +0800

    feat:Programmatic Navigation with useNavigate

--- 11-world-wise/src/components/CountryList.jsx
+++ 11-world-wise/src/components/CountryList.jsx
@@ -1,30 +1,31 @@
-import styles from './CountryList.module.css'
+import styles from "./CountryList.module.css"
 
-import Spinner from '@/components/Spinner'
-import Message from '@/components/Message'
-import CountryItem from '@/components/CountryItem'
-
+import Spinner from "@/components/Spinner"
+import Message from "@/components/Message"
+import CountryItem from "@/components/CountryItem"
 
 export default function CountryList({ cities, isLoading }) {
-    if (isLoading) return <Spinner />
+  if (isLoading) return <Spinner />
 
-    if (!cities.length) {
-        return (
-            <Message message="Add your first city by clicking on a city on the map" />
-        )
+  if (!cities.length) {
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    )
+  }
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country)) {
+      return [...arr, { country: city.country, emoji: city.emoji }]
+    } else {
+      return arr
     }
+  }, [])
 
-    const countries = cities.reduce((arr, city) => {
-        if (!arr.map(el => el.country).includes(city.country)) {
-            return [...arr, { country: city.country, emoji: city.emoji }]
-        } else {
-            return arr
-        }
-    }, [])
-
-    return <ul className={styles.countryList}>
-        {countries.map((country) => (
-            <CountryItem country={country} />
-        ))}
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.country} />
+      ))}
     </ul>
+  )
 }
--- 11-world-wise/src/components/Map.jsx
+++ 11-world-wise/src/components/Map.jsx
@@ -1,15 +1,17 @@
-import { useParams, useSearchParams } from "react-router-dom"
+import { useNavigate, useParams, useSearchParams } from "react-router-dom"
 
 import styles from "./Map.module.css"
 
 export default function Map() {
+  const navigate = useNavigate()
+
   const { id } = useParams()
   const [searchParams, setSearchParams] = useSearchParams()
   const lat = searchParams.get("lat")
   const lng = searchParams.get("lng")
 
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <h1>
         Map
         <p>
