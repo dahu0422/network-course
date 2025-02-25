058661920b9583ac388d85fd8f92c84efc9c9b3e
Dahu-0712 <419460476@qq.com>
2025-02-25 19:30:21 +0800

    feat:Dynamic Routes With URL Parameters

--- 11-world-wise/src/App.jsx
+++ 11-world-wise/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react"
 
 import Homepage from "@/pages/Homepage"
 import AppLayout from "@/pages/AppLayout"
@@ -7,10 +7,11 @@ import Product from "@/pages/Product"
 import Pricing from "@/pages/Pricing"
 import Login from "@/pages/Login"
 import CityList from "@/components/CityList"
+import City from "@/components/City"
 import CountryList from "@/components/CountryList"
 import PageNotFound from "@/pages/PageNotFound"
 
-const BASE_URL = 'http://localhost:9000'
+const BASE_URL = "http://localhost:9000"
 
 function App() {
   const [cities, setCities] = useState([])
@@ -24,7 +25,7 @@ function App() {
         const data = await res.json()
         setCities(data)
       } catch (error) {
-        alert('There was an error loading data')
+        alert("There was an error loading data")
       } finally {
         setIsLoading(false)
       }
@@ -40,9 +41,19 @@ function App() {
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
--- 11-world-wise/src/components/CityItem.jsx
+++ 11-world-wise/src/components/CityItem.jsx
@@ -1,20 +1,23 @@
-import styles from './CityItem.module.css'
+import styles from "./CityItem.module.css"
+import { Link } from "react-router-dom"
 
 const formatDate = (date) =>
-    new Intl.DateTimeFormat('en', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric'
-    }).format(new Date(date))
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date))
 
 export default function CityItem({ city }) {
-    const { cityName, emoji, date } = city;
-    return (
-        <li className={styles.cityItem}>
-            <span className={styles.emoji}>{emoji}</span>
-            <h3 className={styles.name}>{cityName}</h3>
-            <time className={styles.date}>{formatDate(date)}</time>
-            <button className={styles.deleteBtn}>&times;</button>
-        </li>
-    )
+  const { cityName, emoji, date, id } = city
+  return (
+    <li>
+      <Link to={`${id}`} className={styles.cityItem}>
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
+    </li>
+  )
 }
