bdd64b695753936e498a62878a4ecb03f6e35d79
Dahu-0712 <419460476@qq.com>
2025-03-01 14:11:38 +0800

    feat:Displaying City Markers on Map

--- 11-world-wise/src/components/Map.jsx
+++ 11-world-wise/src/components/Map.jsx
@@ -1,26 +1,49 @@
-import { useNavigate, useParams, useSearchParams } from "react-router-dom"
+// import { useParams, useSearchParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { useCities } from "@/contexts/CitiesContext"
+import AMapLoader from "@amap/amap-jsapi-loader"
 
 import styles from "./Map.module.css"
 
 export default function Map() {
-  const navigate = useNavigate()
+  let map = null
+  const { cities } = useCities()
+  console.log(cities)
 
-  const { id } = useParams()
-  const [searchParams, setSearchParams] = useSearchParams()
-  const lat = searchParams.get("lat")
-  const lng = searchParams.get("lng")
+  useEffect(() => {
+    if (cities.length === 0) return // 如果 cities 为空，则不执行地图初始化逻辑
+    window._AMapSecurityConfig = {
+      securityJsCode: "8679ae5f87fcadc81fd97be150999c52",
+    }
+    AMapLoader.load({
+      key: "2ef4a96cfd68274daf2efe76a3092656",
+      version: "2.0",
+      plugins: [],
+    })
+      .then((AMap) => {
+        map = new AMap.Map("mapContainer", {
+          viewMode: "3D",
+          zoom: 11,
+          center: [116.3912757, 39.906217],
+        })
+        //构造点标记
+        cities.forEach((city) => {
+          const { lat, lng } = city.position
+          var marker = new AMap.Marker({
+            icon: "https://webapi.amap.com/theme/v1.3/markers/n/mark_b.png",
+            position: [lng, lat],
+          })
+          //单独将点标记和矢量圆形添加到地图上
+          map.add(marker)
+        })
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+    return () => {
+      map?.destroy()
+    }
+  }, [cities]) // 添加 cities 作为依赖项
 
-  return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>
-        Map
-        <p>
-          Position: {lng}, {lat}
-        </p>
-        <button onClick={() => setSearchParams({ lat: 23, lng: 25 })}>
-          Change Position
-        </button>
-      </h1>
-    </div>
-  )
+  return <div className={styles.mapContainer} id="mapContainer" />
 }
