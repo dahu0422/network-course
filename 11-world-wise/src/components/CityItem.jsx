5eb2314f198e4974d678b2905c031e59568511ec
dahu0422 <149865122+dahu0422@users.noreply.github.com>
2025-03-03 05:40:41 +0000

    feat:Deleting a City

--- 11-world-wise/src/components/CityItem.jsx
+++ 11-world-wise/src/components/CityItem.jsx
@@ -12,8 +12,14 @@ const formatDate = (date) =>
   }).format(new Date(date))
 
 export default function CityItem({ city }) {
-  const { currentCity } = useCities()
+  const { currentCity, deleteCity } = useCities()
   const { cityName, emoji, date, id, position } = city
+
+  function handleDeleteCity(e) {
+    e.preventDefault()
+    deleteCity(id)
+  }
+
   return (
     <li>
       <Link
@@ -23,7 +29,7 @@ export default function CityItem({ city }) {
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn} onClick={handleDeleteCity}>&times;</button>
       </Link>
     </li>
   )
--- 11-world-wise/src/contexts/CitiesContext.jsx
+++ 11-world-wise/src/contexts/CitiesContext.jsx
@@ -57,9 +57,23 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true)
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      })
+      setCities((cities) => cities.filter((city) => city.id !== id))
+    } catch (error) {
+      alert("There was an error deleteing the city...", error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <CitiesContext.Provider
-      value={{ cities, isLoading, currentCity, getCity, createCity }}
+      value={{ cities, isLoading, currentCity, getCity, createCity, deleteCity }}
     >
       {children}
     </CitiesContext.Provider>
