98a5cf20dc2749581f9789651c1d31250bd0c172
dahu0422 <149865122+dahu0422@users.noreply.github.com>
2025-03-05 13:42:02 +0000

    feat:Optimizing Context Re-Renders

--- 12-atomic-blog/src/PostContext.js
+++ 12-atomic-blog/src/PostContext.js
@@ -1,49 +1,61 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useMemo, useState } from "react";
+import { faker } from "@faker-js/faker";
 
-const PostContext = createContext()
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
+// 1) CREATE A CONTEXT
+const PostContext = createContext();
 
 function PostProvider({ children }) {
-    const [posts, setPosts] = useState(() =>
-        Array.from({ length: 30 }, () => createRandomPost())
-    );
-    const [searchQuery, setSearchQuery] = useState("");
-    const [isFakeDark, setIsFakeDark] = useState(false);
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
 
-    // Derived state. These are the posts that will actually be displayed
-    const searchedPosts =
-        searchQuery.length > 0
-            ? posts.filter((post) =>
-                `${post.title} ${post.body}`
-                    .toLowerCase()
-                    .includes(searchQuery.toLowerCase())
-            )
-            : posts;
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
 
-    function handleAddPost(post) {
-        setPosts((posts) => [post, ...posts]);
-    }
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
 
-    function handleClearPosts() {
-        setPosts([]);
-    }
+  function handleClearPosts() {
+    setPosts([]);
+  }
 
-    return <PostContext.Provider value={{
-        posts: searchedPosts,
-        onAddPost: handleAddPost,
-        onClearPosts: handleClearPosts,
-        searchQuery,
-        setSearchQuery,
-    }}>
-        {{ children }}
-    </PostContext.Provider>
+  const value = useMemo(() => {
+    return {
+      posts: searchedPosts,
+      onAddPost: handleAddPost,
+      onClearPosts: handleClearPosts,
+      searchQuery,
+      setSearchQuery,
+    };
+  }, [searchedPosts, searchQuery]);
+
+  return (
+    // 2) PROVIDE VALUE TO CHILD COMPONENTS
+    <PostContext.Provider value={value}>{children}</PostContext.Provider>
+  );
 }
 
 function usePosts() {
-    const context = useContext(PostContext)
-    if(context === undefined) {
-        throw new Error('PostContext was used outside ot the PostProvider')
-    }
-    return context
+  const context = useContext(PostContext);
+  if (context === undefined)
+    throw new Error("PostContext was used outside of the PostProvider");
+  return context;
 }
 
-export { usePosts, PostProvider }
+export { PostProvider, usePosts };
