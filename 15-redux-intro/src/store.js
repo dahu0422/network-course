28665ecccaec29b60fe6a6e241e45226b7160296
dahu0422 <149865122+dahu0422@users.noreply.github.com>
2025-03-23 08:38:07 +0000

    feat:Creating the Store With RTK

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,14 +1,24 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { thunk } from 'redux-thunk' // 处理异步操作, redux 本身只能处理同步操作
+// import { createStore, combineReducers, applyMiddleware } from 'redux'
+// import { thunk } from 'redux-thunk' // 处理异步操作, redux 本身只能处理同步操作
+
+import { configureStore } from '@reduxjs/toolkit'
+
 import accountReducer from './features/accounts/accountSlice'
 import customerReducer from './features/customers/customerSlice'
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer
-})
+// const rootReducer = combineReducers({
+//   account: accountReducer,
+//   customer: customerReducer
+// })
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+// const store = createStore(rootReducer, applyMiddleware(thunk))
+
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer
+  }
+})
 
 export default store
 
