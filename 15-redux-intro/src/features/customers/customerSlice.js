21d96bb8860652a79d59469c113235b0f232c642
dahu0422 <149865122+dahu0422@users.noreply.github.com>
2025-04-23 13:18:02 +0000

    feat:Creating the Customer Slice

--- 15-redux-intro/src/features/customers/customerSlice.js
+++ 15-redux-intro/src/features/customers/customerSlice.js
@@ -1,33 +1,63 @@
-const initialStateCustomer = {
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
   fullName: '',
   nationalID: '',
   createdAt: '',
 }
 
-export default function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createdAt: new Date().toISOString()
+const customerSlice = createSlice({
+  name: 'customer',
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: { fullName, nationalID }
+        }
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName
+        state.nationalID = action.payload.nationalID
+        state.createdAt = new Date().toISOString()
       }
-    case "customer/updateCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-      }
-    default:
-      return state
+    },
+
+    updateCustomer(state, action) {
+      state.fullName = action.payload.fullName
+      state.nationalID = action.payload.nationalID
+    }
   }
-}
+})
 
-export function createCustomer(fullName, nationalID) {
-  return { type: 'customer/createCustomer', payload: { fullName, nationalID } }
-}
+export const { createCustomer, updateCustomer } = customerSlice.actions
 
-export function updateCustomer(fullName, nationalID) {
-  return { type: 'customer/updateCustomer', payload: { fullName, nationalID } }
-}
+export default customerSlice.reducer
+
+// export default function customerReducer(state = initialState, action) {
+//   switch (action.type) {
+//     case "customer/createCustomer":
+//       return {
+//         ...state,
+//         fullName: action.payload.fullName,
+//         nationalID: action.payload.nationalID,
+//         createdAt: new Date().toISOString()
+//       }
+//     case "customer/updateCustomer":
+//       return {
+//         ...state,
+//         fullName: action.payload.fullName,
+//         nationalID: action.payload.nationalID,
+//       }
+//     default:
+//       return state
+//   }
+// }
+
+// export function createCustomer(fullName, nationalID) {
+//   return { type: 'customer/createCustomer', payload: { fullName, nationalID } }
+// }
+
+// export function updateCustomer(fullName, nationalID) {
+//   return { type: 'customer/updateCustomer', payload: { fullName, nationalID } }
+// }
--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,24 +1,13 @@
-// import { createStore, combineReducers, applyMiddleware } from 'redux'
-// import { thunk } from 'redux-thunk' // 处理异步操作, redux 本身只能处理同步操作
+import { configureStore } from "@reduxjs/toolkit";
 
-import { configureStore } from '@reduxjs/toolkit'
-
-import accountReducer from './features/accounts/accountSlice'
-import customerReducer from './features/customers/customerSlice'
-
-// const rootReducer = combineReducers({
-//   account: accountReducer,
-//   customer: customerReducer
-// })
-
-// const store = createStore(rootReducer, applyMiddleware(thunk))
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
 
 const store = configureStore({
   reducer: {
     account: accountReducer,
-    customer: customerReducer
-  }
-})
+    customer: customerReducer,
+  },
+});
 
-export default store
-
+export default store;
